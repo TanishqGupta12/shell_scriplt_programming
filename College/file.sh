4a5ae8b728e9496771acec2fafd200def7727142
TanishqGupta <76784859+TanishqGupta12@users.noreply.github.com>
2022-10-18 15:30:27 +0530

    Add files via upload

---
+++ College/file.sh
@@ -0,0 +1,8 @@
+echo " enter file name "
+read b
+if test -e $b
+then
+echo " file exist "
+else
+echo "fie not exist"
+fi 
---
+++ College/string.sh
@@ -0,0 +1,11 @@
+echo "enter string one"
+read a
+echo " enter string two"
+read b
+if test $a==$b 
+then
+
+echo " string is equal"
+else
+echo " string is not equal "
+fi 
